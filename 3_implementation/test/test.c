7a07f4ee672becce6b9a4256c0122fd2d64de22d
vineelbattula00 <86198474+vineelbattula00@users.noreply.github.com>
2021-07-15 00:18:45 +0530

    Update test.c

--- 3_implementation/test/test.c
+++ 3_implementation/test/test.c
@@ -1,1 +1,41 @@
+#include "unity.h"
+#include "structures.h"
+#include "1.h"
+#include "2.h"
+#include "3.h"
+#include "4.h"
+#include "5.h"
+#include "6.h"
+
+/* Modify these two lines according to the project */
+#include <lms_operations.h>
+#define PROJECT_NAME    "grocery billing system"
+
+/* Prototypes for all the test functions */
+void test_login(void);
+
+
+/* Required by the unity test framework */
+void setUp(){}
+/* Required by the unity test framework */
+void tearDown(){}
+
+/* Start of the application test */
+int main()
+{
+/* Initiate the Unity Test Framework */
+  UNITY_BEGIN();
+
+/* Run Test functions */
+  RUN_TEST(test_login);
+
+
+  /* Close the Unity Test Framework */
+  return UNITY_END();
+}
+
+/* Write all the test functions */ 
+void test_add(void) {
+  TEST_ASSERT_EQUAL(SUCCESS, login("user id", "password"));
+}
 
